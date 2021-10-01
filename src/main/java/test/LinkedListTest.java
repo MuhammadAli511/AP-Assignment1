1fe9957a510270da9758fa5c1e7bef44a955d6b7
MuhammadAli511 <mohammadali417@outlook.com>
2021-10-01 22:54:37 +0500

    Completed Linked List Test Cases

--- src/main/java/test/LinkedListTest.java
+++ src/main/java/test/LinkedListTest.java
@@ -16,12 +16,111 @@ public class LinkedListTest {
 		list1.pushFront(10);
 		list1.pushFront(20);
 		list1.pushFront(30);
+		list1.pushFront(40);
+		list1.pushFront(50);
+		List<Integer> selfMade = new ArrayList<Integer>();
+		selfMade.add(50);
+		selfMade.add(40);
+		selfMade.add(30);
+		selfMade.add(20);
+		selfMade.add(10);
+		Node iterationNode = list1.head;
+		int i = 0;
+		while (iterationNode != null) {
+			Assert.assertEquals(iterationNode.key,selfMade.get(i).intValue());
+			iterationNode = iterationNode.next;
+			i++;
+		}
+	}
+	
+	@Test
+	public void pushBackTest1() {
+		LinkedList list1 = new LinkedList();
+		list1.pushBack(10);
+		list1.pushBack(20);
+		list1.pushBack(30);
+		list1.pushBack(40);
+		list1.pushBack(50);
 		List<Integer> selfMade = new ArrayList<Integer>();
 		selfMade.add(10);
 		selfMade.add(20);
 		selfMade.add(30);
-		Node test = list1.head;
-		test.key = 1;
+		selfMade.add(40);
+		selfMade.add(50);
+		Node iterationNode = list1.head;
+		int i = 0;
+		while (iterationNode != null) {
+			Assert.assertEquals(iterationNode.key,selfMade.get(i).intValue());
+			iterationNode = iterationNode.next;
+			i++;
+		}
 	}
-
+	
+	@Test
+	public void popFrontTest1() {
+		LinkedList list1 = new LinkedList();
+		list1.pushBack(10);
+		list1.pushBack(20);
+		list1.pushBack(30);
+		list1.pushBack(40);
+		list1.pushBack(50);
+		List<Integer> selfMade = new ArrayList<Integer>();
+		selfMade.add(20);
+		selfMade.add(30);
+		selfMade.add(40);
+		selfMade.add(50);
+		list1.popFront();
+		Node iterationNode = list1.head;
+		int i = 0;
+		while (iterationNode != null) {
+			Assert.assertEquals(iterationNode.key,selfMade.get(i).intValue());
+			iterationNode = iterationNode.next;
+			i++;
+		}
+	}
+	@Test
+	public void popBackTest1() {
+		LinkedList list1 = new LinkedList();
+		list1.pushBack(10);
+		list1.pushBack(20);
+		list1.pushBack(30);
+		list1.pushBack(40);
+		list1.pushBack(50);
+		List<Integer> selfMade = new ArrayList<Integer>();
+		selfMade.add(10);
+		selfMade.add(20);
+		selfMade.add(30);
+		selfMade.add(40);
+		list1.popBack();
+		Node iterationNode = list1.head;
+		int i = 0;
+		while (iterationNode != null) {
+			Assert.assertEquals(iterationNode.key,selfMade.get(i).intValue());
+			iterationNode = iterationNode.next;
+			i++;
+		}
+	}
+	
+	@Test
+	public void popTest1() {
+		LinkedList list1 = new LinkedList();
+		list1.pushBack(10);
+		list1.pushBack(20);
+		list1.pushBack(30);
+		list1.pushBack(40);
+		list1.pushBack(50);
+		List<Integer> selfMade = new ArrayList<Integer>();
+		selfMade.add(10);
+		selfMade.add(20);
+		selfMade.add(40);
+		selfMade.add(50);
+		list1.pop(2);
+		Node iterationNode = list1.head;
+		int i = 0;
+		while (iterationNode != null) {
+			Assert.assertEquals(iterationNode.key,selfMade.get(i).intValue());
+			iterationNode = iterationNode.next;
+			i++;
+		}
+	}
 }
