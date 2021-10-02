d296b97effa21114b35ebc1fc4e907a45114785e
MuhammadAli511 <mohammadali417@outlook.com>
2021-10-02 17:13:41 +0500

    Completed Queue Implementation

--- src/test/java/test/QueueArrayTest.java
+++ src/test/java/test/QueueArrayTest.java
@@ -5,7 +5,6 @@ import org.junit.Assert;
 import org.junit.Test;
 
 
-
 public class QueueArrayTest {
 
 	@Test
@@ -24,12 +23,37 @@ public class QueueArrayTest {
 		selfMade.add(50);
 		int i = queue1.removePointer;
 		int j = 0;
-		while(i != queue1.array.length - 1) {
+		while(i != queue1.addPointer - 1) {
 			Assert.assertEquals(queue1.array[i],selfMade.get(j).intValue());
 			i++;
 			j++;
 		}
 	}
+	@Test
+	public void enqueueTest2() {
+		QueueArray queue1 = new QueueArray(5);
+		queue1.enqueue(10);
+		queue1.enqueue(20);
+		queue1.enqueue(30);
+		queue1.enqueue(40);
+		queue1.enqueue(50);
+		queue1.dequeue();
+		queue1.enqueue(60);
+		List<Integer> selfMade = new ArrayList<Integer>();
+		selfMade.add(60);
+		selfMade.add(20);
+		selfMade.add(30);
+		selfMade.add(40);
+		selfMade.add(50);
+		
+		int j = 0;
+		while(j != queue1.array.length-1) {
+			Assert.assertEquals(queue1.array[j],selfMade.get(j).intValue());
+			j++;
+		}
+		
+	}
+	
 	@Test
 	public void dequeueTest1() {
 		QueueArray queue1 = new QueueArray(5);
@@ -40,17 +64,17 @@ public class QueueArrayTest {
 		queue1.enqueue(50);
 		queue1.dequeue();
 		queue1.dequeue();
+		queue1.dequeue();
+		queue1.dequeue();
+		queue1.dequeue();
+		queue1.enqueue(10);
+		System.out.println(queue1.removePointer);
+		System.out.println(queue1.addPointer);
 		List<Integer> selfMade = new ArrayList<Integer>();
-		selfMade.add(30);
-		selfMade.add(40);
-		selfMade.add(50);
+		selfMade.add(10);
 		int i = queue1.removePointer;
 		int j = 0;
-		while(i != queue1.array.length - 1) {
-			Assert.assertEquals(queue1.array[i],selfMade.get(j).intValue());
-			i++;
-			j++;
-		}
+		Assert.assertEquals(queue1.array[i],selfMade.get(0).intValue());
 	}
 
 }
--- src/test/java/test/QueueImplementationTest.java
+++ src/test/java/test/QueueImplementationTest.java
@@ -7,8 +7,104 @@ import org.junit.Test;
 public class QueueImplementationTest {
 
 	@Test
-	public void enqueueTest1() {
-		
+	public void enqueueTest1() throws Exception{
+		QueueImplementation queue1 = new QueueImplementation(5);
+		queue1.enqueue(10);
+		queue1.enqueue(20);
+		queue1.enqueue(30);
+		queue1.enqueue(40);
+		queue1.enqueue(50);
+		List<Integer> selfMade = new ArrayList<Integer>();
+		selfMade.add(10);
+		selfMade.add(20);
+		selfMade.add(30);
+		selfMade.add(40);
+		selfMade.add(50);
+		int i = queue1.removePointer;
+		int j = 0;
+		while(i != queue1.addPointer - 1) {
+			Assert.assertEquals(queue1.array[i],selfMade.get(j).intValue());
+			i++;
+			j++;
+		}
 	}
+	
+	@Test
+	public void enqueueTest2() throws Exception{
+		QueueImplementation queue1 = new QueueImplementation(5);
+		queue1.enqueue(10);
+		queue1.enqueue(20);
+		queue1.enqueue(30);
+		queue1.enqueue(40);
+		queue1.enqueue(50);
+		queue1.dequeue();
+		queue1.enqueue(60);
+		List<Integer> selfMade = new ArrayList<Integer>();
+		selfMade.add(60);
+		selfMade.add(20);
+		selfMade.add(30);
+		selfMade.add(40);
+		selfMade.add(50);
+		
+		int j = 0;
+		while(j != queue1.array.length-1) {
+			Assert.assertEquals(queue1.array[j],selfMade.get(j).intValue());
+			j++;
+		}
+	}
+	
+	@Test
+	public void dequeueTest1() throws Exception{
+		QueueImplementation queue1 = new QueueImplementation(5);
+		queue1.enqueue(10);
+		queue1.enqueue(20);
+		queue1.enqueue(30);
+		queue1.enqueue(40);
+		queue1.enqueue(50);
+		queue1.dequeue();
+		queue1.dequeue();
+		queue1.dequeue();
+		queue1.dequeue();
+		queue1.dequeue();
+		queue1.enqueue(10);
+		System.out.println(queue1.removePointer);
+		System.out.println(queue1.addPointer);
+		List<Integer> selfMade = new ArrayList<Integer>();
+		selfMade.add(10);
+		int i = queue1.removePointer;
+		int j = 0;
+		Assert.assertEquals(queue1.array[i],selfMade.get(0).intValue());
+	}
+	
+	@Test
+	public void emptyTest1() throws Exception{
+		QueueImplementation queue1 = new QueueImplementation(1);
+		queue1.enqueue(10);
+		queue1.dequeue();
+		Assert.assertTrue(queue1.empty());
+	}
+	
+	@Test
+	public void emptyTest2() throws Exception{
+		QueueImplementation queue1 = new QueueImplementation(1);
+		queue1.enqueue(10);
+		Assert.assertFalse(queue1.empty());
+	}
+	
+	@Test
+	public void sizeTest1() throws Exception{
+		QueueImplementation queue1 = new QueueImplementation(1);
+		queue1.enqueue(10);
+		queue1.dequeue();
+		Assert.assertEquals(0,queue1.size());
+	}
+	
+	@Test
+	public void sizeTest2() throws Exception{
+		QueueImplementation queue1 = new QueueImplementation(1);
+		queue1.enqueue(10);
+		Assert.assertEquals(1,queue1.size());
+	}
+	
 
 }
